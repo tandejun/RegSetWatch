f5b5134c7ba248a62ef74e627078773c1f15c49a
kurozzxc <74487494+kurozzxc@users.noreply.github.com>
2025-11-20 17:16:15 +0800

    Update frida_setup.bat
    hidden

--- frida_setup.bat
+++ frida_setup.bat
@@ -31,8 +31,7 @@ echo.>>"C:\Scripts\startup_frida.bat"
 echo cd /d "C:\Users\ICT3215\Desktop">>"C:\Scripts\startup_frida.bat"
 echo.>>"C:\Scripts\startup_frida.bat"
 
-echo start /b "" "frida-server-17.5.1-windows-x86_64.exe">>"C:\Scripts\startup_frida.bat"
-echo timeout /t 2 ^>nul>>"C:\Scripts\startup_frida.bat"
+echo powershell -WindowStyle Hidden -Command "Start-Process 'C:\Users\ICT3215\Desktop\frida-server-17.5.1-windows-x86_64.exe' -WindowStyle Hidden">>"C:\Scripts\startup_frida.bat"
 echo.>>"C:\Scripts\startup_frida.bat"
 
 echo :WAIT_LOOP>>"C:\Scripts\startup_frida.bat"
@@ -43,7 +42,7 @@ echo ^    goto WAIT_LOOP>>"C:\Scripts\startup_frida.bat"
 echo )>>"C:\Scripts\startup_frida.bat"
 echo.>>"C:\Scripts\startup_frida.bat"
 
-echo start /b "" py -3 "frida_local.py">>"C:\Scripts\startup_frida.bat"
+echo start "" "C:\Users\ICT3215\AppData\Local\Programs\Python\Python313\pythonw.exe" "frida_local.py">>"C:\Scripts\startup_frida.bat"
 echo endlocal>>"C:\Scripts\startup_frida.bat"
 echo exit /b>>"C:\Scripts\startup_frida.bat"
 
@@ -52,12 +51,20 @@ echo exit /b>>"C:\Scripts\startup_frida.bat"
 echo [+] Created: C:\Scripts\startup_frida.bat
 echo.
 
+echo [+] Writing startup_frida.vbs...
+
+echo Set WshShell = CreateObject("WScript.Shell")>>"C:\Scripts\startup_frida.vbs"
+echo WshShell.Run "C:\Scripts\startup_frida.bat", 0, False>>"C:\Scripts\startup_frida.vbs"
+
+echo [+] Created: C:\Scripts\startup_frida.vbs
+echo.
+
 :: 3. Register scheduled task (runs on startup, hidden)
 echo [+] Registering task...
 
 schtasks /create ^
   /tn "FridaAutoStart" ^
-  /tr "C:\Scripts\startup_frida.bat" ^
+  /tr "wscript.exe \"C:\Scripts\startup_frida.vbs\"" ^
   /sc onlogon ^
   /rl highest ^
   /f
